c67af9fbf9af441d61743d52180619daa3fac7c3
Diptanil Biswas <153063236+Dipnaba123@users.noreply.github.com>
2025-08-19 20:00:34 +0530

    Update available

--- R/available_layers.R
+++ R/available_layers.R
@@ -6,6 +6,6 @@
 #' available_layers()
 #' @export
 available_layers <- function() {
-  vec <- fromJSON("http://127.0.0.1:8000/get_vec")
+  vec <- fromJSON("http://172.15.1.21:8000/get_vec")
   vec
 }
