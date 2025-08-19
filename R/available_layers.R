d8aaa9acdd89f02fec57b933c75377ddf2468abd
Diptanil Biswas <153063236+Dipnaba123@users.noreply.github.com>
2025-08-19 22:40:45 +0530

    push

--- R/available_layers.R
+++ R/available_layers.R
@@ -6,6 +6,22 @@
 #' available_layers()
 #' @export
 available_layers <- function() {
-  vec <- fromJSON("http://172.15.1.21:8000/get_vec")
-  vec
+  base_url <- "http://172.15.1.21:8000"
+  res <- GET(url = paste0(base_url, "/get_vec"))
+  fromJSON(res)
+}
+
+#' Available Datasets
+#'
+#' A function in R that retrieves a summary of SNP-gene-tissue associations from the Genotype-Tissue Expression GTEx dataset.
+#' @param layer_vector  a vector of the layers
+#' @return Summary Statistics of the SNP,Gene and Tissue
+#' @examples 
+#' layer_dataset <- c("annotation_layers","epigenomic","G_E","genomic","transcriptomic")
+#' available_datasets(layer_dataset)
+#' @export
+available_datasets <- function() {
+  base_url <- "http://172.15.1.21:8000"
+  res <- GET(url = paste0(base_url, "/available_datasets_server"), query = list(layer_dataset = layer_dataset))
+  fromJSON(res)
 }
